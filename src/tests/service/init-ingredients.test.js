73a7b5988a443d94df98785ede81359d3a4b086e
pkarasev <p.karasev@psw.ru>
2024-03-05 16:02:00 +0300

    upd

--- src/tests/service/init-ingredients.test.js
+++
@@ -1,8 +0,0 @@
-import store from "../../services/redux"
-
-describe('redux state tests', () => {
-  it('Redux init ingredients', () => {
-    const state = store.getState()
-    expect(state.ingredients.list).toEqual([])
-  })
-})
---
+++ src/tests/service/redux-reducers.test.js
@@ -0,0 +1,105 @@
+import store from "../../services/redux"
+
+describe('Redux state tests', () => {
+  
+  const state = store.getState()
+
+  it('Examle', () => {
+    expect(state.ingredients.list).toEqual([])
+  })
+
+
+  it('getIngredientsThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+
+  it('updateOrder', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('deleteFromOrder', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('resortOrder', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('closeOrderError', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('sendOrderSend', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('sendOrderThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('removeApiError', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('sendRegisterThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('sendLoginThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('sendLogoutThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('getProfileThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('updateProfileThunk', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('wsError', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('wsFeedConnect', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('updateFeedOrders', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('wsHistoryConnect', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+  it('updateHistoryOrders', () => {
+    // const state = store.getState()
+    // expect(state.ingredients.list).toEqual([])
+  })
+
+
+
+})
